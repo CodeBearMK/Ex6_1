5d8b66aea2a04cf04ef041f2abfd26e0a9d82f85
cat32tw <mike1993xd@gmail.com>
2022-02-28 22:28:13 +0800

    第六章習題1

---
+++ Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ex6_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                label4.Text = Convert.ToString(Convert.ToDouble(textBox1.Text) * 3.3057) + "平方公尺";
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+}
